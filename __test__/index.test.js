52d4fb48fc53e2e2660ae0000ecd02f2568bdf6e
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-11-16 10:06:59 +0700

    test: Catch all errors

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -10,24 +10,24 @@ describe('Rainbow', () => {
     const { GraphQLServer } = require('../')
     const schema = require('./schemas')
     const graphQLServer = new GraphQLServer(baseURL, schema, { graphiqlEnabled: true })
-    await graphQLServer.start()
+    await graphQLServer.start().catch(console.error)
 
     // Broker, you need this only if you're broker provider
     const { Broker } = require('../')
     const broker = new Broker(brokerURI)
-    await broker.start()
+    await broker.start().catch(console.error)
 
     // Worker, you need this only if you want to provide some service which is GraphQL in this case
     const graphqlURI = `${baseURL}/graphql`
     const { Worker } = require('../')
     const worker = new Worker(brokerURI, serviceName)
     worker.initGraphQL(graphqlURI)
-    await worker.start()
+    await worker.start().catch(console.error)
 
     // Client, you need this if you want to fetch something from worker
     const { Client } = require('../')
     const client = new Client(brokerURI, serviceName)
-    await client.start()
+    await client.start().catch(console.error)
 
     // Mutate from client
     const mutationResult = await client.fetch({ query: `mutation { setFoo(bar: "world!") }` }).catch(console.error)
